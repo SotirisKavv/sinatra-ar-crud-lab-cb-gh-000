a4ccc00abba511461292ac2c4ffd3729a3ecdb4a
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-26 20:40:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(title:params[:title], content:params[:content])
+    redirect to '/articles/#{article.id}'
+  end
+
+  get '/articles/:id' do
+    erb :show
   end
 end
